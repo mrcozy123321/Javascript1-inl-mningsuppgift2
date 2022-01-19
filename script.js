4407be032c0700b9c68f4f81f6e6d3964cd52da7
Daniel Mrkoci <daniel@wiklunds.nu>
2022-01-19 08:09:26 +0100

    finished with css styling, to start with JS

---
+++ script.js
@@ -0,0 +1,9 @@
+const form = document.querySelector('#form');
+const addTodoTitle = document.querySelector('#addTodo-title');
+const addTodoText = document.querySelector('#addTodo-text');
+const cardCheck = document.querySelector('#item-check');
+const removeBtn = document.querySelector('#btn-remove');
+const addBtn = document.querySelector('#btn-add');
+const output = document.querySelector('#item-container');
+const counter = document.querySelector('list-counter');
+
